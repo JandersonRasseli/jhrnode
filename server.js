2e5c40bac66e4ce45b0c244d2f4063890ccc906d
JandersonRasseli <janderson.rasseli@gmail.com>
2019-11-29 10:48:21 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('Jandinho no NODE!!');
 });
 
-var porta = process.env.PORT || 8000;
+var porta = process.env.PORT || 3001;
 app.listen(porta);
