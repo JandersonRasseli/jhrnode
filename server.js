78875f2d531f7372020cb89363d58c35c97046de
Janderson Rasseli <janderson.rasseli@gmail.com>
2020-01-03 16:51:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(cors());
+const fetch = require('node-fetch')
+
+const url = 'http://177.91.195.245/'
+let valor = ''
+const request = async () => {
+  const response = await fetch(url);
+  const retorno = await response.text();
+
+  valor = retorno.slice(retorno.search("<h3>")+4, retorno.search("</h3>"))
+  console.log(valor)
+}
 
 app.get('/', function (req, res) {
-  res.send('Janderson no NODE!!');
+  request();
+  res.send("<html> "+
+           "  <head> "+
+           "    <meta charset= \'UTF-8\' http-equiv=\'refresh\' content=2 /> "+
+           "  </head> "+
+           "  <body> "+
+           "    <div> "+
+           "      <h1>Nível 1</h1> "+
+           "      <div>"+valor+"</div> "+
+           "    </div> "+
+           "  </body> "+
+           "</html>")
 });
 
-var porta = process.env.PORT || 3001;
-app.listen(porta);
+var porta = process.env.PORT || 3001
+app.listen(porta)
