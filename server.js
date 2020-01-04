809a063aee0c279ad7e7b4c536e92624be4df187
Janderson Rasseli <janderson.rasseli@gmail.com>
2020-01-04 10:14:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const app = express();
 
-/*const fetch = require('node-fetch')
+let valor = 0
 
-const url = 'http://177.91.195.245:8080'
-//const url = 'http://172.17.210.225/' 
-let valor = ''
-const request = async () => {
-  const response = await fetch(url);
-  const retorno = await response.text();
-
-  valor = retorno.slice(retorno.search("<h3>")+4, retorno.search("</h3>"))
-  console.log(valor)
-}.query
-
-*/
+app.get('/esp', function (req, res) {
+  valor = req.query.vl
+  res.send("OK")
+});
 
 app.get('/', function (req, res) {
-  //request();
   res.send("<html> "+
            "  <head> "+
            "    <meta charset= \'UTF-8\' http-equiv=\'refresh\' content=2 /> "+
            "  </head> "+
            "  <body> "+
            "    <div> "+
-           "      <h1>Nível 3</h1> "+
-           "      <div>"+req.query.valor+"</div> "+
+           "      <h1>Nível 4</h1> "+
+           "      <div>"+valor+"</div> "+
            "    </div> "+
            "  </body> "+
            "</html>")
