56e8256573ef3402f3f0c03e0b01a8f414099c3b
Janderson Rasseli <janderson.rasseli@gmail.com>
2020-01-07 15:16:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let valor = 0
 app.get('/esp', function (req, res) {
   valor = req.query.vl
   console.log(req.query)
-  res.end('Ok')
+  res.end(req.query)
 });
 
 app.get('/', function (req, res) {
