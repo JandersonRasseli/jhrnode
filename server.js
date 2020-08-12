f8bb083201f07401eb3d1560c867acf86b305e82
Janderson Rasseli <janderson.rasseli@gmail.com>
2020-08-11 21:28:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.post('/atual', function (req, res) {
 });
 
 app.get('/ver', function (req, res) {
-  if (valor == "LLL")
     res.send(" <html> "+
              "  <head> "+
              "    <meta charset= \'UTF-8\' http-equiv=\'refresh\' content=10 /> "+
@@ -29,53 +28,10 @@ app.get('/ver', function (req, res) {
              "  <body> "+
              "    <div> "+
              "      <h1>Nível</h1> "+
-             "      <div>Colocar imagem 1 aqui </div> "+
+             "      <div>"+valor+"</div> "+
              "    </div> "+
              "  </body> "+
              "</html>")
-  
-  if (valor == "LLD")
-    res.send(" <html> "+
-             "  <head> "+
-             "    <meta charset= \'UTF-8\' http-equiv=\'refresh\' content=10 /> "+
-             "    <title>Nível de Água</title> "+
-             "  </head> "+
-             "  <body> "+
-             "    <div> "+
-             "      <h1>Nível</h1> "+
-             "      <div>Colocar imagem 2 aqui </div> "+
-             "    </div> "+
-             "  </body> "+
-             "</html>")
-  
-  if (valor == "LDD")
-    res.send(" <html> "+
-             "  <head> "+
-             "    <meta charset= \'UTF-8\' http-equiv=\'refresh\' content=10 /> "+
-             "    <title>Nível de Água</title> "+
-             "  </head> "+
-             "  <body> "+
-             "    <div> "+
-             "      <h1>Nível</h1> "+
-             "      <div>Colocar imagem 3 aqui </div> "+
-             "    </div> "+
-             "  </body> "+
-             "</html>")
-  
-  if (valor == "DDD")
-    res.send(" <html> "+
-             "  <head> "+
-             "    <meta charset= \'UTF-8\' http-equiv=\'refresh\' content=10 /> "+
-             "    <title>Nível de Água</title> "+
-             "  </head> "+
-             "  <body> "+
-             "    <div> "+
-             "      <h1>Nível</h1> "+
-             "      <div>Colocar imagem 4 aqui </div> "+
-             "    </div> "+
-             "  </body> "+
-             "</html>")
-  
 });
 
 var porta = process.env.PORT || 3001
