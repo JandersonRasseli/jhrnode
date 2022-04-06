240a0e425d41158afe2cfd548fe080f3eaab65e3
Janderson Rasseli <janderson.rasseli@gmail.com>
2022-04-06 15:15:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ let valor = 0
 app.use(express.static(__dirname + '/public'))
 
 app.get('/alt', function (req, res) {
-  valor = req.query.vl
+  //valor = req.query.vl
+  valor = 100
+
   console.log(req.query)
   res.end(req.query.vl)
 });
